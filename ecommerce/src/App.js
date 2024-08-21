3ab0757f7430903fdad04876f4f3095da26a4473
Prakash0m <omprakash099507@gmail.com>
2024-08-21 17:21:37 +0545

    firstclass react

---
+++ ecommerce/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import First from "./Components/First";
+import Second from "./Components/Second";
+
+function App() {
+  return (
+    <>
+      <First />
+    </>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/Components/First.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const First = () => {
+  return (
+    <>
+      <h1>Heading first</h1>
+    </>
+  );
+};
+
+export default First;
